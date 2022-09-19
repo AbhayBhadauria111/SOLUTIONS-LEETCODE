82e4b539d90e042c6c8917d4b85866acf918ea74
AbhayBhadauria111 <86351418+AbhayBhadauria111@users.noreply.github.com>
2022-09-19 19:19:07 +0530

    Time: 0 ms (100.00%), Space: 6 MB (51.40%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        int i=0;
+        while(i<32)
+        {
+            uint32_t bit=((n>>i)& 1)? 1:0;
+            ans=ans<<1;
+            ans=ans^bit;
+            i++;
+        }
+        return ans;
+    }
+};

49a3606747e97420e0f74fda75c2ce0fa834c083
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2022-10-11 09:14:06 +0530

    Time: 64 ms (15.10%), Space: 13.8 MB (96.45%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome)==1:
+            return ''
+        else:
+            for i in range(len(palindrome)//2):
+                if palindrome[i]!='a':
+                    return palindrome[:i]+'a'+palindrome[i+1:]
+            return palindrome[:-1]+'b'
+                    
+                    
+                    
+                    
+                    
+            
+        
+                
+                
+            
+        

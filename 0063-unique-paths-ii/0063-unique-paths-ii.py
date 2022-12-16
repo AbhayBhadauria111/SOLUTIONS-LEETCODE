c9607958bbac4a65e58285bab4598e7f023ae0ea
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2022-12-16 14:55:15 +0530

    Time: 38 ms (98.46%), Space: 13.9 MB (97.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -2,24 +2,27 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m=len(obstacleGrid)
         n=len(obstacleGrid[0])
-        def solve(i,j):
-            if D.get((i,j))!=None:
-                return D[(i,j)]
-            if i==m-1 and j==n-1:
-                return 1
-            elif obstacleGrid[i][j]==1:
-                return 0
-            else:
-                a=b=0
-                if j<n-1:
-                    a=solve(i,j+1)
-                if i<m-1:
-                    b=solve(i+1,j)
-                D[(i,j)]=a+b
-                return a+b
-        D={}
-        if obstacleGrid[m-1][n-1]==1:
+        if obstacleGrid[0][0]==1:
             return 0
-        return solve(0,0)
+        obstacleGrid[0][0]=1
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[0])):
+                if i==0 and j==0:
+                    continue
+                if obstacleGrid[i][j]==1:
+                    obstacleGrid[i][j]=0
+                    continue
+                if i==0:
+                    obstacleGrid[i][j]=obstacleGrid[i][j-1]
+                    continue
+                elif j==0:
+                    obstacleGrid[i][j]=obstacleGrid[i-1][j]
+                    continue
+                else:
+                    obstacleGrid[i][j]=obstacleGrid[i-1][j]+obstacleGrid[i][j-1]
+        return obstacleGrid[-1][-1]
+                    
+                    
+                    
         
         

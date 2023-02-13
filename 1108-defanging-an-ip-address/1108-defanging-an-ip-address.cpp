b10170ebd0be17feb5d5c9793bb50ff23dcf18fb
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-02-13 18:04:19 +0530

    Time: 3 ms (41.90%), Space: 5.9 MB (91.63%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans="";
+        for(char i:address)
+        {
+            if(i=='.')ans+="[.]";
+            else ans+=i;
+        }
+        return ans;
+    }
+};

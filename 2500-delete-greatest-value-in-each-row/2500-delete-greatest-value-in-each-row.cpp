df559f86111a435bdd2bbe132fb4da7aebadd4b1
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-04-07 14:32:25 +0530

    Time: 18 ms (35.95%), Space: 9.6 MB (27.61%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+        int ans=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            sort(grid[i].begin(),grid[i].end());
+        }
+        for(int i=0;i<grid[0].size();i++)
+        {
+            int temp=0;
+            for(int j=0;j<grid.size();j++)
+                temp=max(temp,grid[j][i]);
+            ans+=temp;
+        }
+        return ans;
+    }
+};

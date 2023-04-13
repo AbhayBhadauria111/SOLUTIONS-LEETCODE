430b3eee5ee5eab8e9ca12aa28f46c63894d8f9e
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-04-13 18:03:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (8.59%) - LeetHub

---
+++ 2481-minimum-cuts-to-divide-a-circle/2481-minimum-cuts-to-divide-a-circle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        if(n==1)return 0;
+        if(n%2==0)
+        {
+            return n/2;
+        }
+        else
+        {
+            return n;
+        }
+    }
+};

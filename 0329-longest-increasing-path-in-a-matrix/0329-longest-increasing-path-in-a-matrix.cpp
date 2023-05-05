4c41c489501ec0c2848b60fb23865fa51b36c2de
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-05-05 18:06:40 +0530

    Time: 218 ms (9.38%), Space: 41.3 MB (8.51%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    vector<vector<bool>>visited;
+    vector<vector<int>>DP;
+    int n,m;
+    vector<vector<int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        n=matrix.size();
+        m=matrix[0].size();
+        visited.resize(n,vector<bool>(m,false));
+        DP.resize(n,vector<int>(m,-1));
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                ans=max(ans,solve(i,j,matrix));
+            }
+        }
+        return ans;
+    }
+private:
+    bool canput(int i,int j,vector<vector<int>>&M,int curr)
+    {
+        if(i<0 or i>=n or j<0 or j>=m or M[i][j]<=curr)return false;
+        return true;
+    }
+    int solve(int i,int j,vector<vector<int>>&M)
+    {
+        if(DP[i][j]!=-1)return DP[i][j];
+        else
+        {
+            int temp=0;
+            for(auto x:dir)
+            {
+                int newx=i+x[0];
+                int newy=j+x[1];
+                if(canput(newx,newy,M,M[i][j]))
+                {
+                    temp=max(temp,solve(newx,newy,M));
+                }
+            }
+            DP[i][j]=1+temp;
+            return DP[i][j];
+        }
+        
+    }
+};

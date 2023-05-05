23f715eda85b80150d087eeaac7555ed0ae3d071
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-05-05 18:33:21 +0530

    Time: 250 ms (18.23%), Space: 71.3 MB (13.79%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,3 +1,13 @@
+class comp{
+public:
+    bool operator()(pair<int,double>A,pair<int,double>B)
+    {
+        if(A.second>=B.second)return false;
+        return true;
+    }
+    
+};
+
 class Solution {
     unordered_map<int,vector<pair<int,double>>>graph;
     void make_graph(const vector<vector<int>>& edges,const vector<double>& succProb)
@@ -8,20 +18,23 @@ class Solution {
             graph[edges[i][1]].push_back(make_pair(edges[i][0],succProb[i]));
         }
     }
+    
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
         make_graph(edges,succProb);
         double ans=0;
         vector<double>prob(n,INT_MIN);
         prob[start]=1;
-        queue<pair<int,double>>Q;
+        // queue<pair<int,double>>Q;
+        priority_queue<pair<int,double>,vector<pair<int,double>>,comp>Q;
         pair<int,double>curr;
         Q.push(make_pair(start,1));
         while(!Q.empty())
         {
-          curr=Q.front();Q.pop();
+          curr=Q.top();Q.pop();
             prob[curr.first]=max(prob[curr.first],curr.second);
             // cout<<curr.first<<" ";
+            if(curr.first==end)return curr.second;
             for(auto mypair:graph[curr.first])
             {
                 double currprob=curr.second*mypair.second;

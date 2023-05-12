200c5298955129d71d972de585697be2c8fcf069
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-05-12 12:11:24 +0530

    Time: 388 ms (40.45%), Space: 115.3 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,23 +1,20 @@
 class Solution {
-    vector<long long>DP;
+    
 public:
     long long mostPoints(vector<vector<int>>& questions) {
-        DP.clear();
-        DP.resize(questions.size(),-1);
-        return solve(0,questions);
-    }
-private:
-    long long solve(int i,vector<vector<int>>& questions)
-    {
-        if(i>=questions.size())return 0;
-        else
+        vector<long long>DP(questions.size()+1,0);
+        long long a,b;
+        for(int i=questions.size()-1;i>=0;i--)
         {
-            if(DP[i]!=-1)return DP[i];
-            long long a=0;
-            long long b=0;                
-            a=solve(i+1,questions);
-            b=questions[i][0]+solve(i+questions[i][1]+1,questions);
-            return DP[i]=max(a,b); 
+            a=DP[i+1];
+            if(i+questions[i][1]<questions.size())
+            {
+                b=questions[i][0]+DP[questions[i][1]+1+i];
+            }
+            else
+                b=questions[i][0];
+            DP[i]=max(a,b);
         }
+        return DP[0];
     }
 };

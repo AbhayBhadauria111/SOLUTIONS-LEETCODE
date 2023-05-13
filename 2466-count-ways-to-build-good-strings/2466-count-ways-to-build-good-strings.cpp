c74e9704e4a073017d0ea82d0243c4522f11946d
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-05-13 11:39:38 +0530

    Time: 13 ms (80.23%), Space: 9.3 MB (92.26%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,23 +1,14 @@
 class Solution {
-    vector<int>DP;
+    ;
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        DP.resize(high+1,-1);
-        int ans=0;
-        ans=solve(0,low,high,zero,one);
-        return ans;
-    }
-private:
-    int solve(int currl,int low, int high,int zero,int one)
-    {
-        if(currl>high)return 0;
-        if(currl==high)return 1;
-        else
+        vector<int>DP(high+1,-1);
+        for(int i=high;i>=0;i--)
         {
-            if(DP[currl]!=-1)return DP[currl];
             int temp=0;
-            if(currl>=low)temp=1;
-            return DP[currl]=(temp+solve(currl+zero,low,high,zero,one)+solve(currl+one,low,high,zero,one))%(1000000007); 
+            if(i>=low)temp++;
+            DP[i]=(temp+((i+zero<=high)?DP[i+zero]:0)+((i+one)<=high?DP[i+one]:0))%(1000000007);
         }
+        return DP[0];
     }
 };

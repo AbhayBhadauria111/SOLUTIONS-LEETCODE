44b2a9325a8294a4887e5a2f6e5351b783e1f3c5
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-05-16 18:03:20 +0530

    Time: 53 ms (10.72%), Space: 16.6 MB (11.41%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,21 +1,16 @@
 class Solution {
-    vector<vector<int>>DP;
 public:
     bool stoneGame(vector<int>& piles) {
-        DP.resize(piles.size(),vector<int>(piles.size(),-1));
-        int s=accumulate(piles.begin(),piles.end(),0);
-        int alice=solve(0,piles.size()-1,piles);
-        if(alice>s/2)return true;
+        vector<vector<int>>DP(piles.size(),vector<int>(piles.size(),INT_MIN));
+        int score=solve(0,piles.size()-1,piles,DP);
+        if(score)return true;
         return false;
     }
 private:
-    int solve(int i,int j,vector<int>&piles)
+    int solve(int i,int j,vector<int>&piles,vector<vector<int>>&DP)
     {
-        if(i>=j)return 0;
-        else
-        {
-            if(DP[i][j]!=-1)return DP[i][j];
-            return DP[i][j]=max({piles[i]+solve(i+2,j,piles),piles[i]+solve(i+1,j-1,piles),piles[j]+solve(i+1,j-1,piles),piles[j]+solve(i,j-2,piles)});
-        }
+        if(i==j)return piles[i];
+        if(DP[i][j]!=INT_MIN)return DP[i][j];
+        return DP[i][j]=max(piles[i]-solve(i+1,j,piles,DP),piles[j]-solve(i,j-1,piles,DP));
     }
 };

59a8d2ec746f63b52a424bc5334c3f910a4b489d
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-06-03 23:42:42 +0530

    Time: 378 ms (32.82%), Space: 123.1 MB (41.56%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,6 +1,8 @@
 class Solution {
+    unordered_map<int,int>DP;
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        DP.clear();
         int ans=0;
         for(int i=0;i<n;i++)
         {
@@ -12,6 +14,7 @@ private:
     int reach_parent(vector<int>& manager, vector<int>& informTime,int curr)
     {
         if(manager[curr]==-1)return 0;
-        else return informTime[manager[curr]]+reach_parent(manager,informTime,manager[curr]);
+        if(DP[curr]!=0)return DP[curr];
+        else return DP[curr]=informTime[manager[curr]]+reach_parent(manager,informTime,manager[curr]);
     }
 };

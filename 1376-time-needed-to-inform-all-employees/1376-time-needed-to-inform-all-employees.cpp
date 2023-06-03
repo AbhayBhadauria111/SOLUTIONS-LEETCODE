2d2a24d31aa71788ee5caaaffb1bfbc985f30259
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-06-03 23:41:28 +0530

    Time: 2508 ms (5.02%), Space: 90.7 MB (98.47%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        unordered_map<int,vector<int>>graph;
+        int ans=0;
         for(int i=0;i<n;i++)
         {
-            graph[manager[i]].push_back(i);
-        }
-        int ans=0;
-        queue<pair<int,int>>Q;
-        Q.push({headID,0});
-        while(!Q.empty())
-        {
-            pair<int,int>curr=Q.front();Q.pop();
-            ans=max(ans,curr.second);
-            for(int x:graph[curr.first])
-            {
-                Q.push({x,curr.second+informTime[curr.first]});
-            }
+            ans=max(ans,reach_parent(manager,informTime,i));
         }
         return ans;
+    }
+private:
+    int reach_parent(vector<int>& manager, vector<int>& informTime,int curr)
+    {
+        if(manager[curr]==-1)return 0;
+        else return informTime[manager[curr]]+reach_parent(manager,informTime,manager[curr]);
     }
 };

c83b473ecac700edb5e1f354ca50fad63c19ef42
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-06-04 14:23:25 +0530

    Time: 93 ms (5.06%), Space: 85.7 MB (5.04%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
-//     USING DISJOINT SET
     int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<int>parent(isConnected.size(),0);
-        vector<int>rank(isConnected.size(),0);
-        for(int i=0;i<isConnected.size();i++)
-            parent[i]=i;
+        int count=0;
+        vector<bool>vis(isConnected.size(),false);
         for(int i=0;i<isConnected.size();i++)
         {
-            for(int j=0;j<isConnected.size();j++)
+            if(!vis[i])
             {
-                if(isConnected[i][j]==1)
-                {
-                    int u=find(i,parent);
-                    int v=find(j,parent);
-                    if(u!=v)
-                    {
-                        if(rank[u]>rank[v])
-                        {
-                            parent[v]=u;
-                        }
-                        else if(rank[v]>rank[u])
-                        {
-                            parent[u]=v;
-                        }
-                        else
-                        {
-                         parent[v]=u;
-                        rank[u]++;
-                        }
-                    }
-                }
+                count++;
+                dfs(i,vis,isConnected);
             }
         }
-        for(int i=0;i<parent.size();i++)parent[i]=find(i,parent);
-        unordered_set<int>S(parent.begin(),parent.end());
-        return S.size();
+        return count;
     }
-private:
-    int find(int node,vector<int>&parent)
+   private:
+    void dfs(int curr,vector<bool>&vis,vector<vector<int>>isCon)
     {
-        if(parent[node]==node)return node;
-        else return parent[node]=find(parent[node],parent);
+        if(vis[curr])return;
+        vis[curr]=true;
+        for(int i=0;i<isCon.size();i++)
+        {
+            if(!vis[i] and isCon[curr][i])dfs(i,vis,isCon);
+        }
     }
 };

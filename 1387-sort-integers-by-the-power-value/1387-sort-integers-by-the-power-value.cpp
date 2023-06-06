2211c6abc4f780b8164c660953fbb9c763b64fc7
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-06-06 12:21:14 +0530

    Time: 29 ms (82.97%), Space: 8.7 MB (85.66%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,51 +1,24 @@
 class Solution {
 private:
-    unordered_map<int,int>power;
-    int getpower(int i,unordered_map<int,int>&power)
+    int pow(int a)
     {
-        if(i==1)return 0;
-        else
+        int count=0;
+        while(a!=1)
         {
-            if(power[i]!=0)return power[i];
-            if(i%2==0)
-            {
-                return power[i]=1+getpower(i/2,power);
-            }
-            else
-            {
-                return power[i]=1+getpower(3*i+1,power);
-            }
+            if(a%2==0)a=a/2;
+            else a=3*a+1;
+            count++;
         }
+        return count;
     }
 public:
     int getKth(int lo, int hi, int k) {
-        vector<int>A;
-        power.clear();
-        power[1]=0;
+        vector<pair<int,int>>A;
         for(int i=lo;i<=hi;i++)
         {
-            A.push_back(i);
-            getpower(i,power);
+            A.push_back({pow(i),i});
         }
-        // for(auto x:A)cout<<x<<" "<<power[x]<<"||";
-        // cout<<endl;
-        for(int i=0;i<k;i++)
-        {
-            int ind=i;
-            for(int j=i+1;j<A.size();j++)
-            {
-                if(power[A[j]]<power[A[ind]])ind=j;
-                else if(power[A[j]]==power[A[ind]])
-                {
-                    if(A[j]<A[ind])ind=j;
-                }
-            }
-            int temp=A[i];
-            A[i]=A[ind];
-            A[ind]=temp;
-        }
-        // cout<<endl;
-        // for(auto x:A)cout<<x<<" ";
-        return A[k-1];
+        sort(A.begin(),A.end());
+        return A[k-1].second;
     }
 };

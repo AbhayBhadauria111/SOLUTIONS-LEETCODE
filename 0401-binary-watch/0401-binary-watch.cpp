c61825cd7011a5118eae7b2f9727b1177403d2a0
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-06-07 16:30:09 +0530

    Time: 2 ms (46.27%), Space: 6.9 MB (6.38%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -10,13 +10,10 @@ private:
     {
         if(ON==0)
         {
-            if(h<12 and m<=59)
+            if(m>=10)ans.push_back(to_string(h)+":"+to_string(m));
+            else
             {
-                if(m>=10 and m<=59)ans.push_back(to_string(h)+":"+to_string(m));
-                else
-                {
-                    ans.push_back(to_string(h)+":0"+to_string(m));
-                }
+                ans.push_back(to_string(h)+":0"+to_string(m));
             }
         }
         else if(ind==10)return; 
@@ -25,11 +22,15 @@ private:
             solve(ind+1,ON,h,m);
             if(ind<=3)
             {
-                solve(ind+1,ON-1,h+(1<<(3-ind)),m);
+                h=h+(1<<(3-ind));
+                if(h<12)
+                solve(ind+1,ON-1,h,m);
             }
             else
             {
-                solve(ind+1,ON-1,h,m+(1<<(9-ind)));
+                m=m+(1<<(9-ind));
+                if(m<60)
+                solve(ind+1,ON-1,h,m);
             }
         }
     }

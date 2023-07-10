4e8acf3641835e931ae78bbe3aa50c53cd63374d
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-07-10 18:18:17 +0530

    Time: 49 ms (9.88%), Space: 22.8 MB (8.16%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        auto x=solve(0,graph);
+        unordered_map<int,vector<vector<int>>>map;
+        auto x=solve(0,graph,map);
         for(auto &temp:x)
         {
             temp.push_back(0);
@@ -10,19 +11,20 @@ public:
         return x;
     }
 private:
-    vector<vector<int>>solve(int curr,vector<vector<int>>& graph)
+    vector<vector<int>>solve(int curr,vector<vector<int>>& graph,unordered_map<int,vector<vector<int>>>&map)
     {
         if(curr==(int)graph.size()-1)return {{}};
+        if(map.find(curr)!=map.end())return map[curr];
         vector<vector<int>>ans;
         for(auto x:graph[curr])
         {
-            vector<vector<int>>temp=solve(x,graph);
+            vector<vector<int>>temp=solve(x,graph,map);
             for(auto &y:temp)
             {
                 y.push_back(x);
                 ans.push_back(y);
             }
         }
-        return ans;
+        return map[curr]=ans;
     }
 };

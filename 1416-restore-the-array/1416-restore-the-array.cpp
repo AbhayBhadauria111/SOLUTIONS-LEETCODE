56976dfa9ae2bce124ce1f4f928ddfae431bb408
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-09-18 18:00:01 +0530

    Time: 31 ms (98.30%), Space: 12.1 MB (84.69%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -1,26 +1,24 @@
 class Solution {
-protected:
-    int solve(int ind,string &s,int k,unordered_map<int,int>&DP)
-    {
-        if(ind==s.size())return 1;
-        else
-        {
-            if(DP.find(ind)!=DP.end())return DP[ind];
-            if(s[ind]=='0')return 0;
-            long long temp=0;
-            int ret=0;
-            for(int i=ind;i<s.size();i++)
-            {
-                temp=temp*10+(s[i]-'0');
-                if(temp>k)break;
-                ret=(ret+solve(i+1,s,k,DP))%(1000000007);
-            }
-            return DP[ind]=(ret)%(1000000007);
-        }
-    }
 public:
     int numberOfArrays(string s, int k) {
-        unordered_map<int,int>DP;
-        return solve(0,s,k,DP);
+        vector<int>DP(s.size()+1,0);
+        DP[s.size()]=1;
+        for(int i=s.size()-1;i>-1;i--)
+        {
+            if(s[i]=='0')DP[i]=0;
+            else
+            {
+                long long temp=0;
+                int ret=0;
+                for(int j=i;j<s.size();j++)
+                {
+                    temp=temp*10+(s[j]-'0');
+                    if(temp>k)break;
+                    ret=(ret+DP[j+1])%(1000000007);  
+                }
+                DP[i]=ret;
+            }
+        }
+        return DP[0];
     }
 };

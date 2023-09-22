a394a2010b3b15eada266ba5f9874837cd97af38
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-09-22 12:25:01 +0530

    Time: 464 ms (11.05%), Space: 12 MB (97.93%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,33 +1,29 @@
 class Solution {
-    float dist(int x1,int y1,int x2,int y2)
+    int blast(int curr,vector<vector<int>>&bombs,vector<bool>&vis)
     {
-        return sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+        int temp=1;
+        vis[curr]=true;
+        for(int i=0;i<bombs.size();i++)
+        {
+            if(!vis[i])
+            {
+            double dist=sqrt(pow((bombs[curr][0]-bombs[i][0]),2) + pow(bombs[curr][1]-bombs[i][1],2));
+                if(dist<=(double)bombs[curr][2])temp+=blast(i,bombs,vis);
+            }
+        }
+        return temp;
     }
 public:
     int maximumDetonation(vector<vector<int>>& bombs) {
-     int ans=1;
-        int n=bombs.size();
-     for(int i=0;i<n;i++)
-     {
-         vector<bool>visited(n,false);
-         visited[i]=true;
-         int count=0;
-         queue<vector<int>>Q;
-         Q.push(bombs[i]);
-         while(!Q.empty())
-         {
-             vector<int>curr=Q.front();Q.pop();count++;
-             for(int j=0;j<n;j++)
-             {
-                 if(!visited[j] and dist(curr[0],curr[1],bombs[j][0],bombs[j][1])<=curr[2])
-                 {
-                     Q.push(bombs[j]);
-                     visited[j]=true;
-                 }
-             }
-         }
-         ans=max(ans,count);
-     }
+        int ans=0;
+        vector<bool>vis(bombs.size(),false);
+        for(int i=0;i<bombs.size();i++)
+        {
+            // vector<bool>vis(bombs.size(),false);
+            fill(vis.begin(),vis.end(),false);
+            ans=max(ans,blast(i,bombs,vis));
+            // cout <<i<<" "<<ans<<endl;
+        }
         return ans;
     }
 };

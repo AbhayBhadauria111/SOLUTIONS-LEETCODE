c71f0739198a93d22e79f28082f35cde91f5caa8
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-09-25 12:27:54 +0530

    Time: 39 ms (14.35%), Space: 26.4 MB (31.33%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,36 +10,40 @@
  * };
  */
 class Solution {
-    TreeNode* create(vector<int>& preorder, vector<int>& inorder,int low,int high,int pre)
+    TreeNode* solve(int left,int right,int start,vector<int>& preorder, vector<int>& inorder)
     {
-        if(low==high){
-            TreeNode* N=new TreeNode();
-            N->val=inorder[low];
-            return N;
+        if(left==right)
+        {
+            TreeNode* Node=new TreeNode(inorder[left]);
+            return Node;
         }
         else
         {
-            TreeNode* N=new TreeNode();
-            for(int j=pre;j<preorder.size();j++)
+            bool flag=false;
+            TreeNode* Node=new TreeNode();
+            for(int j=start;j<preorder.size();j++)
             {
-                int x=preorder[j];
-                for(int i=low;i<=high;i++)
+                for(int i=left;i<=right;i++)
                 {
-                    if(inorder[i]==x)
+                    if(preorder[j]==inorder[i])
                     {
-                        N->val=x;
-                        N->left=create(preorder,inorder,low,i-1,j+1);
-                        N->right=create(preorder,inorder,i+1,high,pre);
-                        return N;
+                        Node->val=inorder[i];
+                        Node->left=solve(left,i-1,j+1,preorder,inorder);
+                        Node->right=solve(i+1,right,j+1,preorder,inorder);
+                        flag=1;
+                        break;
                     }
                 }
+                if(flag)break;
             }
-            return nullptr;
+            if(!flag)return nullptr;
+            return Node;
+            
         }
     }
+    
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if(inorder.size()==0)return nullptr;
-        return create(preorder,inorder,0,inorder.size()-1,0);
+        return solve(0,inorder.size()-1,0,preorder,inorder);
     }
 };

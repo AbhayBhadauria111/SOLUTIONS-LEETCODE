da4beb9db77cb9913e92b124ab5213f5485b9740
Abhay Bhadauria <86351418+AbhayBhadauria111@users.noreply.github.com>
2023-09-29 11:51:22 +0530

    Time: 18 ms (98.35%), Space: 34.5 MB (8.44%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution1 {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
         if(root1==nullptr and root2==nullptr)return nullptr;
@@ -23,6 +23,28 @@ public:
             root1->left=mergeTrees(root1->left,root2->left);
             root1->right=mergeTrees(root1->right,root2->right);
             return root1;
+        }
+    }
+};
+// EVERY NODE IS NEW NODE
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1==nullptr and root2==nullptr)return nullptr;
+        else
+        {
+            TreeNode* Node=new TreeNode();
+            if(root1)Node->val=root1->val;
+            if(root2)Node->val+=root2->val;
+            TreeNode*left1=(root1)?root1->left:nullptr;
+            TreeNode*left2=(root2)?root2->left:nullptr;
+            TreeNode*right1=(root1)?root1->right:nullptr;
+            TreeNode*right2=(root2)?root2->right:nullptr;
+            Node->left=mergeTrees(left1,left2);
+            Node->right=mergeTrees(right1,right2);
+            // cout<<Node->val<<endl;
+
+            return Node;
         }
     }
 };
